b058c5c27f7cd92d24115a7417ce52f055fd84aa
thermitegod <thermitethegod@gmail.com>
2018-11-09 18:04:07 -0700

    updates

--- postsync.d/git-prune.sh
+++ postsync.d/git-prune.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-#portage
-cd /usr/portage && git prune
-#layman
-#cd /usr/layman/bliss-overlay && git prune
-#cd /usr/layman/libressl && git prune
-#cd /usr/layman/pg_overlay && git prune
-#cd /usr/layman/science && git prune
-#cd /usr/layman/ && git prune
-#cd /usr/layman/ && git prune
+cd /var/db/repos/gentoo && git prune
